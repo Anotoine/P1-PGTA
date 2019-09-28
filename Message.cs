859bba7e3e7510f18e0f3a6889acee6ab0b1a431
alex3697 <alex3697@hotmail.com>
2019-09-28 14:48:24 +0200

    REVOLUCION

--- Message.cs
+++ Message.cs
@@ -187,12 +187,19 @@ namespace P1_PGTA
                 Offset += 3;
             }
             if (this.listFSPEC[4])
-            {   //TODO: not finished
+            {   //TODO: not finished    DEBE HABER UNA FUNCION, YO NO LA HE ENCONTRADO, HE VISTO Q COGIENDO DATOS EN HEX ASI DEBERIA IR
                 int lat = Int32.Parse(string.Concat(this.rawList[Offset], this.rawList[Offset + 1], this.rawList[Offset + 2], this.rawList[Offset + 3]), System.Globalization.NumberStyles.HexNumber);
+
+
                 int lon = Int32.Parse(string.Concat(this.rawList[Offset + 4], this.rawList[Offset + 5], this.rawList[Offset + 6], this.rawList[Offset + 7]), System.Globalization.NumberStyles.HexNumber);
-                d = new DataItem("I020/041", "Position in WGS-84 Coordinates",new Atom("",0,""));
+
+
+
+                d = new DataItem("I020/041", "Position in WGS-84 Coordinates", new Atom("", 0, ""));
                 Offset += 8;
             }
+
+
             if (this.listFSPEC[5])
             {
                 int x = Int32.Parse(string.Concat(this.rawList[Offset], this.rawList[Offset + 1], this.rawList[Offset + 2]), System.Globalization.NumberStyles.HexNumber);
@@ -204,37 +211,84 @@ namespace P1_PGTA
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
             if (this.listFSPEC[6])
             {
-
+                int matricula = Int32.Parse(string.Concat(this.rawList[Offset], this.rawList[Offset + 1]), System.Globalization.NumberStyles.HexNumber);
+                d = new DataItem("I020/161", "Track Number", new Atom("Track num: ", 1, Convert.ToString(matricula)));
+                Offset += 2;
             }
+
+
+
+
             if (this.listFSPEC[7])
             {
+                d = new DataItem("I020/170", "Track Status");
+                var ls = new List<string> { "CNF", "TRE", "CST", "CDM", "MAH", "STH", "FX","GHO","FX" };
+                var ls0 = new List<string> { "Confirmed track", "Default", "Not extrapolated", "Maintaining", "Climbing", "Default", "Measured position", "End of Data Item", "Default","","","","","","", "End of Data Item" };
+                var ls1 = new List<string> { "Track in initiation phase", "Last report for a track", "Extrapolated", "Descending", "Invalid", "Horizontal manoeuvre", "Smoothed position", "Extension into first extent", "Ghost track", "", "", "","","","", "Extension into second extent" };
+
+                int cont1 = 0;
+                int cont2 = 0;
+                int i = 0;
+                bool exit = false;
+                while (!exit)
+                {
+                    string s = Convert.ToString(Convert.ToInt32(this.rawList[Offset], 16), 2).PadLeft(8, '0');
+                    for (int j = 0; j < 8; j++)
+                    {
+                        if (j == 3 && i == 0)
+                        {
+                            if (char.Equals(s[j], '1'))
+                            {
+                                j++;
+                                if (char.Equals(s[j], '1'))
+                                    a = new Atom(ls[cont1], 1, ls1[cont2 + 1]);
+                                else
+                                    a = new Atom(ls[cont1], 1, ls1[cont2]);
+                            }
+                            else
+                            {
+                                j++;
+                                if (char.Equals(s[j], '1'))
+                                    a = new Atom(ls[cont1], 1, ls1[cont2 + 1]);
+                                else
+                                    a = new Atom(ls[cont1], 1, ls1[cont2]);
+                            }
+                            cont2++;
+                        }
+                        else if (char.Equals(s[j], '1'))
+                            a = new Atom(ls[cont1], 1, ls1[cont2]);
+                        else
+                            a = new Atom(ls[cont1], 1, ls0[cont2]);
+                        
+                        cont1++;
+                        cont2++;
+                        d.addAtom(a);
+
+                        if (j == 7)
+                        {
+                            Offset++;
+                            if (char.Equals(s[j], '0'))
+                                exit = true;
+                            if (char.Equals(s[j], '1'))
+                                i += 1;
+                        }
+                    }
+                }
+                listDataItem.Add(d);
 
             }
+
+
             if (this.listFSPEC[8])
             {
-
+                d = new DataItem("I020/070", "Mode-3/A Code in Octal Representation");
             }
+            
+            
+            
+            
             if (this.listFSPEC[9])
             {
 
