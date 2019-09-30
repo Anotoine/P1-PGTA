09a50d1132c685bc98d378adadba933b3a4597d7
Anotoine <antoni.eritja@hotmail.com>
2019-10-01 00:01:48 +0200

    Adding final touches to be able to have maps of (and only of) BCN airport. Adding Clear button to clear out the canvas

--- Map.xaml.cs
+++ Map.xaml.cs
@@ -31,84 +31,118 @@ namespace P1_PGTA
         {
             Line l;
             Polyline p;
-            string[] lines = File.ReadAllLines(@"BCN_Pistas.map");
-            List<Point> points = new List<Point>();
 
-            int j = 0;
-            while (j < lines.Length)
+            Point zero0 = new Point(41.318936, 2.028951); //x y superior esquerra ?
+
+            double A = -zero0.X;
+            double B = -zero0.Y;
+            double alpha = A / (Lienzo.Width / 2);
+            double beta = B / (Lienzo.Height / 2);
+
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Map Extension files (*.map)|*.map| Text Files (*.txt)|*.txt";
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.InitialDirectory = @"";
+
+            if (openFileDialog.ShowDialog() == true)
             {
-                string[] l1 = lines[j].Split();
-                if (l1[0] == "Linea")
+                string[] lines = File.ReadAllLines(openFileDialog.FileName);
+                List<Point> points = new List<Point>();
+
+                points.Add(new Point(41.296531, 2.075594)); //ARP BCN airport --> Item 0
+
+                int j = 0;
+                while (j < lines.Length)
                 {
-                    for (int i = 1; i < 4; i += 2)
+                    string[] l1 = lines[j].Split();
+                    if (l1[0] == "Linea")
                     {
-                        float a1 = Convert.ToSingle(l1[i].Substring(0, 2)); // grados
-                        float b1 = Convert.ToSingle(l1[i].Substring(2, 2)); // minutos
-                        float c1 = Convert.ToSingle(l1[i].Substring(4, 2)); // segundos
-                        float d1 = Convert.ToSingle(l1[i].Substring(6, 3)); // milisegundos
+                        for (int i = 1; i < 4; i += 2)
+                        {
+                            float a1 = Convert.ToSingle(l1[i].Substring(0, 2)); // grados
+                            float b1 = Convert.ToSingle(l1[i].Substring(2, 2)); // minutos
+                            float c1 = Convert.ToSingle(l1[i].Substring(4, 2)); // segundos
+                            float d1 = Convert.ToSingle(l1[i].Substring(6, 3)); // milisegundos
 
-                        float x1 = a1 + (b1 / 60) + ((c1 + d1 / 1000) / 3600);
+                            float x1 = a1 + (b1 / 60) + ((c1 + d1 / 1000) / 3600);
 
-                        float a2 = Convert.ToSingle(l1[i + 1].Substring(0, 3)); // grados
-                        float b2 = Convert.ToSingle(l1[i + 1].Substring(3, 2)); // minutos
-                        float c2 = Convert.ToSingle(l1[i + 1].Substring(5, 2)); // segundos
-                        float d2 = Convert.ToSingle(l1[i + 1].Substring(7, 3)); // milisegundos
+                            float a2 = Convert.ToSingle(l1[i + 1].Substring(0, 3)); // grados
+                            float b2 = Convert.ToSingle(l1[i + 1].Substring(3, 2)); // minutos
+                            float c2 = Convert.ToSingle(l1[i + 1].Substring(5, 2)); // segundos
+                            float d2 = Convert.ToSingle(l1[i + 1].Substring(7, 3)); // milisegundos
 
-                        float x2 = a2 + (b2 / 60) + ((c2 + d2 / 1000) / 3600);
+                            float x2 = a2 + (b2 / 60) + ((c2 + d2 / 1000) / 3600);
 
-                        points.Add(new Point(x1, x2));
+                            points.Add(new Point(x1, x2));
+                        }
+
+                        l = new Line();
+                        l.Stroke = Brushes.White;
+                        l.StrokeThickness = 1;
+
+                        Point a = points[points.Count - 1];
+                        l.X1 = (a.X + A) / alpha;
+                        l.Y1 = (a.Y + B) / beta;
+
+                        Point b = points[points.Count - 2];
+                        l.X2 = (b.X + A) / alpha;
+                        l.Y2 = (b.Y + B) / beta;
+
+                        Lienzo.Children.Add(l);
+                        j++;
                     }
-
-                    l = new Line();
-                    l.Stroke = Brushes.White;
-                    l.StrokeThickness = 1;
-                    Point a = points[points.Count - 1];
-                    l.X1 = a.X * 1000000 + 500;
-                    l.Y1 = a.Y * 1000000 + 250;
-
-                    Point b = points[points.Count - 2];
-                    l.X2 = b.X * 1000000 + 500;
-                    l.Y2 = b.Y * 1000000 + 250;
-                    Lienzo.Children.Add(l);
-                    j++;
-                }
-                else if (l1[0].StartsWith("Polilinea"))
-                {
-                    int num = Convert.ToInt32(l1[1]);
-                    PointCollection pp = new PointCollection();
-
-                    for (int i = j; i < j + num; i++)
+                    else if (l1[0].StartsWith("Polilinea"))
                     {
-                        string[] l2 = lines[i + 1].Split();
+                        int num = Convert.ToInt32(l1[1]);
+                        PointCollection pp = new PointCollection();
 
-                        float a1 = Convert.ToSingle(l2[0].Substring(0, 2)); // grados
-                        float b1 = Convert.ToSingle(l2[0].Substring(2, 2)); // minutos
-                        float c1 = Convert.ToSingle(l2[0].Substring(4, 2)); // segundos
-                        float d1 = Convert.ToSingle(l2[0].Substring(6, 3)); // milisegundos
+                        for (int i = j; i < j + num; i++)
+                        {
+                            string[] l2 = lines[i + 1].Split();
 
-                        float x1 = a1 + (b1 / 60) + ((c1 + d1 / 1000) / 3600);
+                            float a1 = Convert.ToSingle(l2[0].Substring(0, 2)); // grados
+                            float b1 = Convert.ToSingle(l2[0].Substring(2, 2)); // minutos
+                            float c1 = Convert.ToSingle(l2[0].Substring(4, 2)); // segundos
+                            float d1 = Convert.ToSingle(l2[0].Substring(6, 3)); // milisegundos
 
-                        float a2 = Convert.ToSingle(l2[1].Substring(0, 3)); // grados
-                        float b2 = Convert.ToSingle(l2[1].Substring(3, 2)); // minutos
-                        float c2 = Convert.ToSingle(l2[1].Substring(5, 2)); // segundos
-                        float d2 = Convert.ToSingle(l2[1].Substring(7, 3)); // milisegundos
+                            float x1 = a1 + (b1 / 60) + ((c1 + d1 / 1000) / 3600);
+
+                            float a2 = Convert.ToSingle(l2[1].Substring(0, 3)); // grados
+                            float b2 = Convert.ToSingle(l2[1].Substring(3, 2)); // minutos
+                            float c2 = Convert.ToSingle(l2[1].Substring(5, 2)); // segundos
+                            float d2 = Convert.ToSingle(l2[1].Substring(7, 3)); // milisegundos
                                                        
-                        float x2 = a2 + (b2 / 60) + ((c2 + d2 / 1000) / 3600);
+                            float x2 = a2 + (b2 / 60) + ((c2 + d2 / 1000) / 3600);
 
-                        points.Add(new Point(x1, x2));
-                        pp.Add(new System.Windows.Point(points[points.Count-1].X * 1000000 + 500, points[points.Count-1].Y * 1000000 + 250));
+                            points.Add(new Point(x1, x2));
+                            pp.Add(new System.Windows.Point((points[points.Count-1].X + A) / alpha, (points[points.Count-1].Y + B) / beta));
+                        }
 
+                        p = new Polyline();
+                        p.Stroke = Brushes.LightGreen;
+                        p.StrokeThickness = 1;
+                        p.Points = pp;
+                        Lienzo.Children.Add(p);
+                        j += num;
                     }
-                    p = new Polyline();
-                    p.Stroke = Brushes.LightGreen;
-                    p.StrokeThickness = 1;
-                    p.Points = pp;
-                    Lienzo.Children.Add(p);
-                    j += num;
+                    else
+                        j++;
                 }
-                else
-                    j++;
+                Ellipse ARP = new Ellipse();
+                ARP.Stroke = Brushes.Red;
+                ARP.Fill = Brushes.Red;
+                ARP.Width = 5;
+                ARP.Height = 5;
+                Lienzo.Children.Add(ARP);
+                Canvas.SetLeft(ARP, ((points[0].X + A) / alpha) - ARP.Width/2);
+                Canvas.SetTop(ARP, ((points[0].Y + B) / beta) - ARP.Height/2);
             }
+        }
+
+        private void Button_clear_Click(object sender, RoutedEventArgs e)
+        {
+            Lienzo.Children.Clear();
         }
     }
 }
--- Point.cs
+++ Point.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace P1_PGTA
 {
@@ -13,10 +9,15 @@ namespace P1_PGTA
         private double y;
         private double z;
 
-        //WGS-84
-        private double lat;
-        private double lon;
-        private double alt;
+        //WGS-84 - Degrees
+        private double latD;
+        private double lonD;
+        private double altD;
+
+        //WGS-84 - Radians
+        private double latR;
+        private double lonR;
+        private double altR;
 
         //Polars
         private double rad;
@@ -24,33 +25,39 @@ namespace P1_PGTA
         private double phi;
 
         //ref lat lon for Lamber projection -> AIP airport
-        private double lat0 = 41.296531*Math.PI/180;
-        private double lon0 = 2.075594 * Math.PI / 180;
+        private double latRef = 41.296531 * Math.PI / 180;
+        private double lonRef = 2.075594 * Math.PI / 180;
+
         //standard latitudes 
         private double lat1 = 40 * Math.PI / 180;
         private double lat2 = 42 * Math.PI / 180;
 
         public Point(double lat, double lon) //
         {
-            this.lat = lat * Math.PI / 180;
-            this.lon = lon * Math.PI / 180;
+            this.latD = lat;
+            this.lonD = lon;
+            this.latR = lat * Math.PI / 180;
+            this.lonR = lon * Math.PI / 180;
             //Lambert conformal conic projection
             double num = Math.Log(Math.Cos(lat1) / Math.Cos(lat2)) / Math.Log(Math.Exp(1));
-            double denum = Math.Log((Math.Tan(Math.PI / 4 + lat2/2))/(Math.Tan(Math.PI / 4 + lat1/2))) / Math.Log(Math.Exp(1));
+            double denum = Math.Log((Math.Tan(Math.PI / 4 + lat2 / 2)) / (Math.Tan(Math.PI / 4 + lat1 / 2))) / Math.Log(Math.Exp(1));
             double n = num / denum;
-            double F = (Math.Cos(lat1) * Math.Pow(Math.Tan(Math.PI / 4 + lat1 / 2),n))/n;
-            double rho = F * Math.Pow(Math.Tan(Math.PI/4+ this.lat / 2), -n);
-            double rho0 = F * Math.Pow(Math.Tan(Math.PI / 4 + lat0 / 2), -n);
-            this.x = rho * Math.Sin(n * (this.lon - lon0));
-            this.y = rho0 - rho * Math.Cos(n * (this.lon - lon0));
+            double F = (Math.Cos(lat1) * Math.Pow(Math.Tan(Math.PI / 4 + lat1 / 2), n)) / n;
+            double rho = F * Math.Pow(Math.Tan(Math.PI / 4 + this.latR / 2), -n);
+            double rho0 = F * Math.Pow(Math.Tan(Math.PI / 4 + latRef / 2), -n);
+            this.x = rho * Math.Sin(n * (this.lonR - lonRef));
+            this.y = rho0 - rho * Math.Cos(n * (this.lonR - lonRef));
         }
 
         public double X { get => x; set => x = value; }
         public double Y { get => y; set => y = value; }
         public double Z { get => z; set => z = value; }
-        public double Lat { get => lat; set => lat = value; }
-        public double Lon { get => lon; set => lon = value; }
-        public double Alt { get => alt; set => alt = value; }
+        public double LatR { get => latR; set => latR = value; }
+        public double LonR { get => lonR; set => lonR = value; }
+        public double AltR { get => altR; set => altR = value; }
+        public double LatD { get => latD; set => latD = value; }
+        public double LonD { get => lonD; set => lonD = value; }
+        public double AltD { get => altD; set => altD = value; }
         public double Rad { get => rad; set => rad = value; }
         public double Theta { get => theta; set => theta = value; }
         public double Phi { get => phi; set => phi = value; }
