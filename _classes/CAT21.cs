9523cd3b21d1f70b48b5d7009591a76afbcd1cdb
Anotoine <antoni.eritja@hotmail.com>
2019-10-28 14:55:30 +0100

    Adding some more files form Alex commit. Message and CAT21

---
+++ _classes/CAT21.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASTERIX
+{
+    class CAT21
+    {
+        internal string DI008 { get; set; }
+        internal List<Atom> DI010 { get; set; }
+        internal List<Atom> DI015 { get; set; }
+        internal List<Atom> DI016 { get; set; }
+        internal List<Atom> DI020 { get; set; }
+        internal List<Atom> DI040 { get; set; }
+        internal List<Atom> DI070 { get; set; }
+        internal Point DI071 { get; set; }
+        internal List<Atom> DI072 { get; set; }
+        internal List<Atom> DI073 { get; set; }
+        internal List<Atom> DI074 { get; set; }
+        internal List<Atom> DI075 { get; set; }
+        internal List<Atom> DI076 { get; set; }
+        internal List<Atom> DI077 { get; set; }
+        internal List<Atom> DI080 { get; set; }
+        internal List<Atom> DI090 { get; set; }
+        internal float DI110 { get; set; }
+        internal float DI130 { get; set; }
+        internal float DI131 { get; set; }
+        internal float DI132 { get; set; }
+        internal DateTime DI140 { get; set; }
+        internal float DI145 { get; set; }
+        internal float DI146 { get; set; }
+        internal float DI148 { get; set; }
+        internal float DI150 { get; set; }
+        internal float DI151 { get; set; }
+        internal float DI152 { get; set; }
+        internal float DI155 { get; set; }
+        internal float DI157 { get; set; }
+        internal float DI160 { get; set; }
+        internal int DI161 { get; set; }
+        internal float DI165 { get; set; }
+        internal List<Atom> DI170 { get; set; }
+        internal List<Atom> DI200 { get; set; }
+        internal List<Atom> DI210 { get; set; }
+        internal string DI220 { get; set; }
+        internal List<Atom> DI230 { get; set; }
+        internal List<Atom> DI250 { get; set; }
+        internal List<Atom> DI260 { get; set; }
+        internal List<Atom> DI271 { get; set; }
+        internal string DI295 { get; set; }
+        internal string DI400 { get; set; }
+    }
+}
